8af72e4140f57029c721586fc8470aada6754903
Felipe Farías Salazar <Fel.fariass@duocuc.cl>
2025-05-12 19:25:56 -0400

    Init Commit

---
+++ src/main/java/model/Admin.java
@@ -0,0 +1,11 @@
+package model;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Admin extends Usuario {
+     {}
+}
---
+++ src/main/java/model/Usuario.java
@@ -0,0 +1,21 @@
+package model;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Usuario {
+
+        private String nombre;
+        private String apellido;
+        private String rut;
+        private int edad;
+        private String tipoUsuario;
+
+
+
+    }
+
+
